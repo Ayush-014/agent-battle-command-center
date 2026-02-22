75a464ec1740b179834bbf141485127a11f392db
Ayush-014 <ayushjoshi8445@gmail.com>
2026-02-22 09:59:13 +0530

    updated sorting logic

--- packages/ui/src/components/main-view/TaskQueue.tsx
+++ packages/ui/src/components/main-view/TaskQueue.tsx
@@ -199,7 +199,7 @@ export function TaskQueue() {
           </div>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
-            {filteredTasks
+            {[...filteredTasks]
               .sort(
                 (a, b) =>
                   (priorityOrder[b.priority] || 0) -
